2cb290f5a9e69cdd79b982359b59e59b15f50e71
Thang Nguyen <thangdn@thanglong.edu.vn>
2018-02-01 16:27:25 +0700

    tao chuong trinh main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+This is a Caro program
+The program was writen by thangdn
+*/
+#include <iostream>
+using namespace std;
+
+int tuna =69;
+int main()
+{
+	int tuna = 20;
+	cout<<::tuna<<endl;
+	return 0;
+}
